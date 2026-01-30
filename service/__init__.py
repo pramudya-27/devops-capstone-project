6cfe55322444970329cc24b5c14077570e7b690b
pramudya-27 <dio2431058@itpln.ac.id>
2026-01-30 09:52:45 -0500

    Added Kubernetes manifests and fixed Talisman HTTPS for OpenShift

--- service/__init__.py
+++ service/__init__.py
@@ -13,7 +13,7 @@ from flask_cors import CORS
 
 # Create Flask application
 app = Flask(__name__)
-talisman = Talisman(app)
+talisman = Talisman(app, force_https=False)
 CORS(app)
 app.config.from_object(config)
 
