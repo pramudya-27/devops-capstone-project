3b4c234c0b254334fe379b6dd4dc9e5cbe85d765
Dio Pramudya <dio2431058@itpln.ac.id>
2026-01-30 03:59:42 -0500

    Added method no allowed

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -175,3 +175,14 @@ class TestAccountService(TestCase):
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
         data = resp.get_json()
         self.assertEqual(len(data), 5)
+
+######################################################################
+    #  T E S T   S A D   P A T H S
+    ######################################################################
+    def test_method_not_allowed(self):
+        """It should not allow an illegal method call"""
+        # Kita mencoba memanggil DELETE pada BASE_URL (/accounts)
+        # Padahal DELETE hanya boleh dilakukan pada endpoint spesifik (/accounts/{id})
+        # Flask harusnya otomatis menolak request ini dengan 405
+        resp = self.client.delete(BASE_URL)
+        self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
